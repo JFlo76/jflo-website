ac65ec478d89844ed297e4ff7768b77a7feac12b
Jason Florence <jason@jflo.io>
2024-10-09 14:00:12 -0700

    Adding Indeed work, prepping for V2

---
+++ V2024/src/js/homepage.js
@@ -0,0 +1,52 @@
+'use strict';
+$(document).ready(function(){
+	// function debounce(func, wait, immediate) {
+	// 	var timeout;
+	// 	return function() {
+	// 		var context = this, args = arguments;
+	// 		var later = function() {
+	// 			timeout = null;
+	// 			if (!immediate) func.apply(context, args);
+	// 		};
+	// 		var callNow = immediate && !timeout;
+	// 		clearTimeout(timeout);
+	// 		timeout = setTimeout(later, wait);
+	// 		if (callNow) func.apply(context, args);
+	// 	};
+	// };	
+	
+	// function isElementInViewport(el) {
+	//     if (typeof jQuery === "function" && el instanceof jQuery) {
+	//         el = el[0];
+	//     }
+
+	//     var rect = el.getBoundingClientRect();
+	//     return (
+	//         rect.top < el.clientHeight / 2 &&
+	//         rect.left >= 0 &&
+	//         rect.bottom > el.clientHeight / 2 &&
+	//         rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+	//     );
+	// }
+	
+
+	// var myEfficientFn = debounce(function() {
+	// 	var aboutSectionNavIndicator_1 = $('#nav-about-1');
+	// 	var aboutSectionNavIndicator_2 = $('#nav-about-2');
+
+	// 	if (isElementInViewport($('#about-1'))) {
+	// 	    aboutSectionNavIndicator_1.addClass('active');
+	// 	} else {
+	// 		aboutSectionNavIndicator_1.removeClass('active');
+	// 	}
+	// 	if (isElementInViewport($('#about-2'))) {
+	// 	    aboutSectionNavIndicator_2.addClass('active');
+	// 	} else {
+	// 		aboutSectionNavIndicator_2.removeClass('active');
+	// 	}
+
+	// }, 250, false);
+	
+	// window.addEventListener('scroll', myEfficientFn);
+	
+});
